8686adbe98d5ec8179e439d4ad3aeec70b9035a0
Mustafa Panjiwala <56435815+mustafapanjiwala@users.noreply.github.com>
2022-06-16 12:40:17 +0530

    added prettier config

--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Montserrat_400Regular } from "@expo-google-fonts/montserrat";
-import { useFonts } from "@use-expo/font";
-import AppLoading from "expo-app-loading";
-import Home from "./src/screens/Home";
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Montserrat_400Regular } from '@expo-google-fonts/montserrat';
+import { useFonts } from '@use-expo/font';
+import AppLoading from 'expo-app-loading';
+import Home from './src/screens/Home';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Reg: Montserrat_400Regular,
-  });
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-  return (
-    <View style={styles.container}>
-      <Home />
-    </View>
-  );
+    let [fontsLoaded] = useFonts({
+        Reg: Montserrat_400Regular
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    return (
+        <View style={styles.container}>
+            <Home />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#495EBB",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#495EBB'
+    }
 });
